cf9cfe131ed1b555adcaf4c9edff52615fdc67bc
iamdylan <43369528+iamdylan@users.noreply.github.com>
2018-10-18 21:36:51 +0530

    updates

--- index.php
+++ index.php
@@ -40,10 +40,15 @@
 	$mail->addAttachment($file);
 
 	if ($mail->send())
-		$msg = "Your email has been sent, thank you.";
+		$msg = '<div class="alert alert-success">
+				  <h4 class="alert-heading">Successful!</h4>
+				  <p class="mb-0">Your email has been sent, thank you. We\'ll get in touch with you shortly.</p>
+				</div>';
 	else
-		$msg = "Please try again.
-		echo $mail->ErrorInfo;﻿";
+		$msg = '<div class="alert alert-warning">
+				  <h4 class="alert-heading">Warning!</h4>
+				  <p class="mb-0">'.$mail->ErrorInfo.'. Please try again.</p>
+				</div>';
 	} 
  ?>
 
@@ -51,26 +56,41 @@
 <html>
 <head>
 	<title>Contact Form</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.min.css">
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 </head>
 <body>
 	<div class="container" style="margin-top: 100px">
-		<div class="row justify-content-center">
-			<div class="col-md-6-offset-3" align="center">
-				<img src=""><br><br>
+		<h1 class="conform">CONTACT FORM<hr class="style-one"></h1>
+		<div class="row justify-content-center">		
 
-				<?php if ($msg != "") echo "$msg<br><br>"; ?>
+
+			<div class="col-md-6-offset-3" align="center" data-aos="zoom-in">
+
+				<div class='card card-body  bg-light mt-2 mb-5'>
+				<img src="assets/images/email2.svg"><br><br>
+
+				<?php if ($msg != "") echo $msg; ?><br><br>
 
 				<form method="post" action="index.php" enctype="multipart/form-data">
 					<input class="form-control" name="subject" placeholder="Subject..."><br>
 					<input class="form-control" type="email" name="email" placeholder="Email..."><br>
 					<textarea placeholder="Message..." class="form-control" name="message"></textarea><br>
 					<input class="form-control-file" type="file" name="attachment"><br>
-					<input class="btn btn-primary" type="submit" name="submit" value="Send">
+					<input class="btn btn-primary btn-block" type="submit" name="submit" value="Send">
 				</form>
 			</div>
 		</div>
+		</div>
 	</div>
+
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script>
+		AOS.init();
+	</script>
+
 </body>
 </html>
 
